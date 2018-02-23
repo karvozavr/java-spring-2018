a0fa729b73dc3a3c477be70753ff92dc2096d87c
Dmitriy Abramov <karvozavr@gmail.com>
2018-02-23 12:53:06 +0300

    Finish implementation

--- Lazy/src/main/java/ru/spbau/mit/karvozavr/lazy/LazyFactory.java
+++ Lazy/src/main/java/ru/spbau/mit/karvozavr/lazy/LazyFactory.java
@@ -1,32 +1,86 @@
 package ru.spbau.mit.karvozavr.lazy;
 
 import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
 
 import java.util.function.Supplier;
 
+/**
+ * Factory producing {@link Lazy} implementation objects.
+ */
 public class LazyFactory {
 
+    /**
+     * Create single-thread {@link Lazy} implementation (faster).
+     *
+     * @param supplier supplier of a value
+     * @param <T>      type of a value
+     * @return single-thread {@link Lazy} implementation supplying value by given supplier
+     */
+    @NotNull
     public static <T> Lazy<T> createLazy(@NotNull Supplier<T> supplier) {
-        return new AbstractLazy<T>(supplier) {
-            @Override
-            public T get() {
-                if (supplier == null)
-                    return value;
-                else
-                    return supplier.get();
-            }
-        };
+        return new SingleThreadLazy<>(supplier);
     }
 
+    /**
+     * Create concurrent {@link Lazy} implementation.
+     *
+     * @param supplier supplier of a value
+     * @param <T>      type of a value
+     * @return concurrent {@code Lazy} implementation supplying value by given supplier
+     */
+    @NotNull
     public static <T> Lazy<T> createConcurrentLazy(@NotNull Supplier<T> supplier) {
-        return null;
+        return new ConcurrentLazy<>(supplier);
     }
 
-    private class ConcurrentLazy<T> implements Lazy<T> {
+    /**
+     * Single-thread {@link Lazy} implementation.
+     *
+     * @param <T> value type
+     */
+    private static class SingleThreadLazy<T> implements Lazy<T> {
+
+        @Nullable
+        private Supplier<T> supplier;
+        @Nullable
+        private T value;
+
+        /**
+         * Constructs {@link SingleThreadLazy} with given supplier.
+         *
+         * @param supplier supplier of a value
+         */
+        public SingleThreadLazy(@NotNull Supplier<T> supplier) {
+            this.supplier = supplier;
+        }
+
+        @Override
+        @Nullable
+        public T get() {
+            if (supplier != null) {
+                value = supplier.get();
+                supplier = null;
+            }
+            return value;
+        }
+    }
+
+    /**
+     * Concurrent {@link Lazy} implementation.
+     *
+     * @param <T> value type
+     */
+    private static class ConcurrentLazy<T> implements Lazy<T> {
 
         private volatile Supplier<T> supplier;
-        private T value = (T) new Object();
+        private volatile T value;
 
+        /**
+         * Constructs {@link ConcurrentLazy} with given supplier.
+         *
+         * @param supplier supplier of a value
+         */
         public ConcurrentLazy(@NotNull Supplier<T> supplier) {
             this.supplier = supplier;
         }
@@ -34,9 +88,11 @@ public class LazyFactory {
         @Override
         public T get() {
             if (supplier != null) {
-                synchronized (value) {
-                    value = supplier.get();
-                    supplier = null;
+                synchronized (this) {
+                    if (supplier != null) {
+                        value = supplier.get();
+                        supplier = null;
+                    }
                 }
             }
 
