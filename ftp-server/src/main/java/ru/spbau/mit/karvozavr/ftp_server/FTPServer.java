052759c533253c008949c6640c4172868546d346
Dmitriy Abramov <karvozavr@gmail.com>
2018-04-15 12:50:21 +0300

    Init

---
+++ ftp-server/src/main/java/ru/spbau/mit/karvozavr/ftp_server/FTPServer.java
@@ -0,0 +1,4 @@
+package ru.spbau.mit.karvozavr.ftp_server;
+
+public class FTPServer {
+}
---
+++ ftp-server/src/main/java/ru/spbau/mit/karvozavr/ftp_server/Main.java
@@ -0,0 +1,13 @@
+package ru.spbau.mit.karvozavr.ftp_server;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var a = new ArrayList<Integer>();
+        a.add(22);
+        System.out.println(a);
+    }
+}
